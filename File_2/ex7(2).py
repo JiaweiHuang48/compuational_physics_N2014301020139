4e64382232ab62a2044702c24c972d3b9f49cf02
黄佳玮 <toby459@users.noreply.github.com>
2016-11-06 21:38:47 +0800

    Create ex7(2).py

---
+++ File_2/ex7(2).py
@@ -0,0 +1,59 @@
+import pylab as pl
+import math
+class two_identical_pendulums:
+    def __init__(self,q=0.5,g=9.8,l=9.8,OmegaD=2.0/3.0,time_step=0.04,FD=0.5,\
+    time_of_duration=60,theta1=0.20,theta2=0.199):
+        self.omega1=[0]
+        self.omega2=[0]
+        self.theta1=[theta1]
+        self.theta2=[theta2]
+        self.q=q
+        self.g=g
+        self.l=l
+        self.OmegaD=OmegaD
+        self.FD=FD
+        self.dtheta=[theta1-theta2]
+        self.t=[0]
+        self.dt=time_step
+        self.time=time_of_duration
+        self.nsteps = int(time_of_duration // time_step + 1)
+        self.line=[math.log(self.dtheta[0])]
+        
+    def calculate(self):
+        for i in range(self.nsteps):
+            self.t.append(self.t[i] + self.dt)
+            
+            temp_omega1=self.omega1[-1]-(self.g/self.l)*\
+            math.sin(self.theta1[-1])*self.dt -self.q*self.omega1[-1]*self.dt +\
+            self.FD*math.sin(self.OmegaD*self.t[-1])*self.dt
+            self.omega1.append(temp_omega1)           
+            temp_theta1=self.theta1[-1]+self.omega1[-1]*self.dt
+            if(temp_theta1>=math.pi):
+                 temp_theta1-=2*math.pi
+            if(temp_theta1<=-math.pi):
+                 temp_theta1+=2*math.pi
+            self.theta1.append(temp_theta1)
+            
+            temp_omega2=self.omega2[-1]-(self.g/self.l)*\
+            math.sin(self.theta2[-1])*self.dt -self.q*self.omega2[-1]*self.dt +\
+            self.FD*math.sin(self.OmegaD*self.t[-1])*self.dt
+            self.omega2.append(temp_omega2)            
+            temp_theta2=self.theta2[-1]+self.omega2[-1]*self.dt
+            while(temp_theta2>=math.pi):
+                 temp_theta2-=2*math.pi
+            while(temp_theta2<=-math.pi):
+                 temp_theta2+=2*math.pi
+            self.theta2.append(temp_theta2)
+            D=abs(self.theta1[-1]-self.theta2[-1])
+            self.dtheta.append(math.log(D))
+            self.line.append(math.log(self.dtheta[0])-0.25*self.t[i])
+           
+                
+    def show_results(self):
+        pl.plot(self.t, self.dtheta)
+        pl.plot(self.t,self.line,'--')
+        pl.show()
+        
+a=two_identical_pendulums()
+a.calculate()
+a.show_results()
